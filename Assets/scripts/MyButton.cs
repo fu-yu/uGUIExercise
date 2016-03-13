0f87381d5ae8441c0ea5160e789f00f773b78bbe
Fu-Yu <yuko@mbpLate2013.local>
2016-03-13 16:29:23 +0900

    add particle

---
+++ Assets/scripts/MyButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("MyButton Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
